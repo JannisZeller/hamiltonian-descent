044ef48ce65214ce05daaaddc9d3371b01b85d1a
jzelle <jannis.zeller@arcor.de>
2022-05-26 09:52:26 +0200

    Kinetic Energy class

--- source/hagrad.py
+++ source/hagrad.py
@@ -1,6 +1,9 @@
 # %% Imports
 #-------------------------------------------------------------------------------
 
+import warnings
+from typing import Callable
+
 import tensorflow as tf
 import tensorflow.keras as keras
 
@@ -12,20 +15,70 @@ import tensorflow.keras as keras
 # %% Implementation
 #-------------------------------------------------------------------------------
 
+class KineticEnergyGradients():
+    """This class provides several functions which serve as kinetic energy gradients in Hagrad.
+    """
+
+    def __init__(self, 
+        power_a: float=2.,
+        power_A: float=1.
+    ):
+        self.power_a = power_a
+        self.power_A = power_A
+
+    @tf.function
+    def classical(p_var: tf.Tensor) -> tf.Tensor:
+        """Classical kinetic energy ||p||^2/2 with gradient p."""
+        return(p_var)
+
+    @tf.function
+    def relativistic(p_var: tf.Tensor) -> tf.Tensor:
+        """Relativistic kinetic energy sqrt( ||p||^2 + 1 )-1 with gradient p/sqrt( ||p||^2 + 1 )"""
+        return(p_var / tf.math.sqrt(tf.math.square(tf.norm(p_var)) + 1.))
+
+    @tf.function
+    def power(self, p_var: tf.Tensor) -> tf.Tensor:
+        """Power kinetic energy (1/A) * ( ||p||^a + 1 )^(A/a) - (1/A) with gradient p * ||p||^(a-2) * ( ||p||^a + 1 )^(A/a-1)"""
+        a = self.power_a
+        A = self.power_A
+        p_norm = tf.norm(p_var)
+        return(p_var*p_norm**(a-2.) * (p_norm**a + 1.)**(A/a-1.))
+
+    @tf.function
+    def power_1norm(self, p_var: tf.Tensor) -> tf.Tensor:
+        """Power kinetic energy (1/A) * ( ||p||^a + 1 )^(A/a) - (1/A) with gradient p * ||p||^(a-2) * ( ||p||^a + 1 )^(A/a-1) where ||p|| is the 1-norm."""
+        a = self.power_a
+        A = self.power_A
+        p_norm = tf.norm(p_var, ord=1)
+        return(tf.math.sign(p_var)*p_norm**(a-1.) * (p_norm**a + 1.)**(A/a-1.))
+
+
 class Hagrad(keras.optimizers.Optimizer):
+
     def __init__(self, 
         epsilon: float=1., 
-        gamma: float=2., 
-        name: str="hagrad", 
-        kinetic_energy: str="relativistic", 
+        gamma:   float=10., 
+        name:    str="hagrad", 
+        kinetic_energy: str="relativistic", # DEPRECATED
+
+        # TODO
+        kinetic_energy_gradient: Callable[[tf.Tensor], tf.Tensor]=KineticEnergyGradients.relativistic,
+
         **kwargs
     ): 
         """Call super().__init__() and use _set_hyper() to store hyperparameters"""
         super().__init__(name, **kwargs)
         self.kinetic_energy = kwargs.get("kinetic_energy", kinetic_energy)
+
+        # TODO
+        self.kinetic_energy_gradient = kwargs.get("kinetic_energy_gradient", kinetic_energy_gradient)
+
         self._set_hyper("epsilon", kwargs.get("lr", epsilon)) 
         self._set_hyper("gamma", kwargs.get("gamma", gamma))
         self._set_hyper("delta", 1. / (1. + kwargs.get("lr", epsilon)*kwargs.get("gamma", gamma) ))
+
+        if self.kinetic_energy_gradient.__name__ == "power_1norm":
+            warnings.warn("The power_1norm kinetic energy gradient typically leads to divergence!")
     
     def _create_slots(self, var_list):
         """For each model variable, create the optimizer variable associated with it.
@@ -45,11 +98,24 @@ class Hagrad(keras.optimizers.Optimizer):
         delta   = self._get_hyper("delta", var_dtype)
         p_var.assign(delta * p_var - epsilon * delta * grad)
 
-        if self.kinetic_energy == "classical":
-            var.assign_add(epsilon * p_var)
+        # TODO
+        var.assign_add(epsilon * self.kinetic_energy_gradient(p_var))
 
-        if self.kinetic_energy == "relativistic":
-            var.assign_add(epsilon * p_var / tf.math.sqrt(tf.math.square(tf.norm(p_var)) + 1.))
+
+        # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+        ## -> It seems like some (small amount of) computation time can be saved, if one does 
+        #  not use conditions or a property to store different kinetic energy gradients.
+        # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+
+        # var.assign_add(epsilon * p_var) 
+
+        ## DEPRECATED
+        # if self.kinetic_energy == "classical":
+        #     var.assign_add(epsilon * p_var)
+
+        # if self.kinetic_energy == "relativistic":
+        #     var.assign_add(epsilon * p_var / tf.math.sqrt(tf.math.square(tf.norm(p_var)) + 1.))
 
     def _resource_apply_sparse(self, grad, var):
         raise NotImplementedError
@@ -58,10 +124,11 @@ class Hagrad(keras.optimizers.Optimizer):
         base_config = super().get_config()
         return {
             **base_config,
-            "epsilon": self._serialize_hyperparameter("epsilon"),
-            "gamma":   self._serialize_hyperparameter("gamma"),
-            "delta":   self._serialize_hyperparameter("delta"),
-            "kinetic_energy": self.kinetic_energy,
+            'epsilon': self._serialize_hyperparameter("epsilon"),
+            'gamma':   self._serialize_hyperparameter("gamma"),
+            'delta':   self._serialize_hyperparameter("delta"),
+            'kinetic_energy': "deprecated", ## DEPRECATED
+            'kinetic_energy_gradient': self.kinetic_energy_gradient.__doc__
         }
 
 #-------------------------------------------------------------------------------
@@ -73,6 +140,6 @@ class Hagrad(keras.optimizers.Optimizer):
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    print("Hagrad-Optimizer implementation source file ")
+    print("Hagrad-Optimizer implementation source file.")
 
 #-------------------------------------------------------------------------------
