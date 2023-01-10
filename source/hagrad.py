5aec5067df4f528145f4bbc39544081ec31654cc
JannisZeller <jannis.zeller@arcor.de>
2023-01-10 19:42:33 +0100

    Deleted some trash that Jannis left in hagrad.py

--- source/hagrad.py
+++ source/hagrad.py
@@ -44,17 +44,8 @@ class KineticEnergyGradients():
         power_func.__doc__ = f"Power kinetic energy (1/{A}) * ( ||p||^{a} + 1 )^({A}/{a}) - (1/{A}) with gradient p * ||p||^({a}-2) * ( ||p||^{a} + 1 )^({A}/{a}-1)"
         return power_func
 
-    @tf.function
-    def power_1norm(self, p_var: tf.Tensor) -> tf.Tensor:
-        """Power kinetic energy (1/A) * ( ||p||^a + 1 )^(A/a) - (1/A) with gradient p * ||p||^(a-2) * ( ||p||^a + 1 )^(A/a-1) where ||p|| is the 1-norm."""
-        a = self.power_a
-        A = self.power_A
-        p_norm = tf.norm(p_var, ord=1)
-        return(tf.math.sign(p_var)*p_norm**(a-1.) * (p_norm**a + 1.)**(A/a-1.))
-
 
 class Hagrad(keras.optimizers.Optimizer):
-    _significant_decimals_hypers: int=4
 
     def __init__(self, 
         epsilon: float=1., 
@@ -63,27 +54,18 @@ class Hagrad(keras.optimizers.Optimizer):
         kinetic_energy_gradient: Callable[[tf.Tensor], tf.Tensor]=KineticEnergyGradients.relativistic,
         p0_mean: float=1.,
         p0_std:  float=2.,
-
-        **kwargs
-    ): 
+        **kwargs): 
         """Call super().__init__() and use _set_hyper() to store hyperparameters"""
         super().__init__(name, **kwargs)
-        self.kinetic_energy_gradient = kwargs.get("kinetic_energy_gradient", kinetic_energy_gradient)
-        self._set_hyper("epsilon", kwargs.get("lr", epsilon)) 
-        self._set_hyper("gamma", kwargs.get("gamma", gamma))
-        self._set_hyper("p0_mean", kwargs.get("p0_mean", p0_mean)) 
-        self._set_hyper("p0_std", kwargs.get("p0_std", p0_std))
-
-        delta =  1. / (1. + kwargs.get("lr", epsilon)*kwargs.get("gamma", gamma) )
-        n = self._significant_decimals_hypers -int(np.floor(np.log10(abs(delta))))-1
-        self._set_hyper("delta", round(delta, n))
-
-        eps_delta = epsilon*delta
-        n = self._significant_decimals_hypers -int(np.floor(np.log10(abs(eps_delta))))-1
-        self._set_hyper("eps_delta", round(eps_delta, n))
-
-        if self.kinetic_energy_gradient.__name__ == "power_1norm":
-            warnings.warn("The power_1norm kinetic energy gradient typically leads to divergence!")
+        self.kinetic_energy_gradient = kinetic_energy_gradient
+        self._set_hyper("epsilon", epsilon) 
+        self._set_hyper("gamma",   gamma)
+        self._set_hyper("p0_mean", p0_mean) 
+        self._set_hyper("p0_std",  p0_std)
+        delta =  1. / (1. + epsilon * gamma )
+        self._set_hyper("delta", delta)
+        eps_delta = epsilon * delta
+        self._set_hyper("eps_delta", eps_delta)
     
 
     def _create_slots(self, var_list):
@@ -109,15 +91,8 @@ class Hagrad(keras.optimizers.Optimizer):
         epsilon = self._get_hyper("epsilon", var_dtype)
         delta   = self._get_hyper("delta", var_dtype)
         eps_delta = self._get_hyper("eps_delta", var_dtype)
-
-
         p_var.assign(delta * p_var - eps_delta * grad)
         var.assign_add(epsilon * self.kinetic_energy_gradient(p_var))
-
-        # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
-        ## -> It seems like some (small amount of) computation time can be saved, if one does 
-        #  not use conditions or a property to store different kinetic energy gradients.
-        # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 
 
     def _resource_apply_sparse(self, grad, var):
